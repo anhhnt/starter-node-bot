407d23f4437a945b58b29180d1a12dbda48ea33f
anhhnt <ntahoang@gmail.com>
2016-05-28 09:40:56 +0700

    Commentted out shitty conversations

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hi!')
 })
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+//controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
+//  bot.reply(message, 'Hello.')
   bot.reply(message, 'It\'s nice to talk to you directly.')
-})
+//})
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
+//controller.hears('.*', ['mention'], function (bot, message) {
+//  bot.reply(message, 'You really do care about me. :heart:')
+//})
 
 controller.hears(['sinh to', 'cafe', 'mua nước'], ['ambient'], function (bot, message) {
   bot.reply(message, 'Sinh tố Đại phúc, gọi số này : 090 6807915, tự mua đi đừng kêu e mua dùm nữa');
@@ -47,26 +47,26 @@ controller.hears(['facebook'], ['direct_mention', 'mention', 'direct_message'], 
   bot.reply(message, 'Face của em nè : https://www.facebook.com/larrie.opt');
 });
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
+//controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+//  var help = 'I will respond to the following messages: \n' +
+//      '`bot hi` for a simple message.\n' +
+//      '`bot attachment` to see a Slack attachment message.\n' +
+//      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+//     '`bot help` to see this again.'
+//  bot.reply(message, help)
+//})
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
+//controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+//  var attachments = [{
+//    fallback: text,
+//    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+//    title: 'Host, deploy and share your bot in seconds.',
+//    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//    title_link: 'https://beepboophq.com/',
+//    text: text,
+//    color: '#7CD197'
+//  }]
 
   bot.reply(message, {
     attachments: attachments
